cd39b27503857931ef84a95752602407504afd87
yasinbc <yasyasinbc@gmail.com>
2025-08-29 19:29:41 +0200

    main_V0, divide fixed

--- divide.c
+++ divide.c
@@ -2,5 +2,10 @@
 #include "divide.h"
 
 float divide_floats(float operand1, float operand2){
-    return operand1 / operand2;
+    if(operand2 != 0){
+        return operand1 / operand2;
+    }else{
+        printf("Error: can't divide by 0\n");
+        return 0;
+    }
 }
